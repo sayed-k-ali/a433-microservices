af2847f695c9606faa97cdd06cdb15bca366527a
Sayed Khaidir Ali <sayed-k-ali@github.com>
2023-12-16 01:42:16 +0700

    add circleci script

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -2,6 +2,6 @@
 
 docker build -t ghcr.io/sayed-k-ali/karsajobs:latest .
 
-echo $GHCR_TOKEN | docker login ghcr.io -u sayed-k-ali --password-stdin
+echo ${GHCR_TOKEN} | docker login ghcr.io -u sayed-k-ali --password-stdin
 
 docker push ghcr.io/sayed-k-ali/karsajobs:latest
